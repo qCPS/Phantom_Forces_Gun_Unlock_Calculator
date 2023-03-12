00e4d537d7c3bf5f078def100ac189ef8ee186e0
qCPS <williambramsey@gmail.com>
2023-03-12 14:35:18 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void calculate(int unlock_credits, int rank, int credits) {
+    if (unlock_credits <= credits) {
+        cout << "You will be able to afford the gun at " << rank << "." << endl;
+        cout << "You will be able to buy it for " << unlock_credits << "." << endl;
+        cout << "You will have " << credits << " credits before buying it." << endl;
+    } else {
+        calculate(unlock_credits - 140, rank + 1, credits + 5 * (rank + 1) + 200);
+    }
+};
+
+int main() {
+    int unlock_rank;
+    int rank;
+    int credits;
+
+    cout << "Enter the unlock rank of the gun: " << endl;
+    cin >> unlock_rank;
+
+    cout << "Enter your current rank: " << endl;
+    cin >> rank;
+
+    cout << "Enter your current credits: " << endl;
+    cin >> credits;
+
+    calculate((unlock_rank - rank) * 140 + 700, rank, credits);
+    return 0;
+}
